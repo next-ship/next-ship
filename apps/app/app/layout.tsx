95797b487340ba5281c1beb3f1b02067d5c616c2
Julian <julian@linkcruit.com>
2025-01-02 19:43:46 -0800

    Try relative path for importing globals

--- apps/app/app/layout.tsx
+++ apps/app/app/layout.tsx
@@ -1,4 +1,4 @@
-import "@repo/ui/globals.css";
+import "../../../packages/ui/src/styles/globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import {
